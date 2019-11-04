2fcdd504f9c3d9b358297420013797d9ef2757df
Alexandra Borge <alexandraroseingram@gmail.com>
2019-11-03 20:45:38 -0500

    Add user sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:name, presence: true)
+  validates(:email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
+  validates(:password, presence: true, length: { minimum: 6 })
 end
