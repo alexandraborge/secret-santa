2479efa7b61f1f7830ff43195105f08f45c879a8
Alexandra Borge <alexandraroseingram@gmail.com>
2019-11-10 17:55:14 -0500

    Add logout func, and refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < ApplicationController
+  helper_method(:logged_in?, :current_user)
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenicate(params[:sessions][:password])
-      login_user(user)
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      login(user)
       redirect_to user
     else
       flash[:errors] = 'Invalid email/password combination'
-      redirect_to session_new_path
+      redirect_to login_path
     end
   end
 
   def destroy
+    logout
+    redirect_to root_url
   end
 end
