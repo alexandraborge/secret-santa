b0ccc0c706605a50e0181b3e668d91c6d2125f9c
Alexandra Borge <alexandraroseingram@gmail.com>
2019-11-10 16:54:09 -0500

    Define current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenicate(params[:sessions][:password])
+      login_user(user)
+      redirect_to user
+    else
+      flash[:errors] = 'Invalid email/password combination'
+      redirect_to session_new_path
+    end
+  end
+
+  def destroy
+  end
 end
