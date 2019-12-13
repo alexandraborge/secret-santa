46a1fd7c07f199921c5073d108daa29e8659a2fa
Alexandra Borge <alexandraroseingram@gmail.com>
2019-12-12 23:33:01 -0500

    Fix start draw bug

--- app/controllers/secret_santa_users_controller.rb
+++ app/controllers/secret_santa_users_controller.rb
@@ -11,26 +11,25 @@ class SecretSantaUsersController < ApplicationController
 
   # Update records by adding a value to the gift receiver column
   def start_draw
-    players = SecretSantaUser.all.select { |s| s[:secret_santa_game_id] == params[:id] }
-    receivers = players
-    players = players.each do |player|
-      pick = receivers.sample
-      if pick != player
-        player.update({gift_receiver: player.user_id})
-      else
-        while pick == player
-          if pick != player
-            player.update({gift_receiver: player.user_id})
-          else
-            pick = receivers.sample
-          end
-        end
+    players = SecretSantaUser.all.select do |s| 
+      s[:secret_santa_game_id] == secret_santa_user_game_params
+    end
+
+    receivers = players.dup.map(&:user_id)
+
+    players.each do |player|
+      gift_receiver = receivers.sample
+
+      while gift_receiver == player.user_id
+        gift_receiver = receivers.sample
       end
-      receivers = receivers - [pick]
+      player.update(gift_receiver: gift_receiver)
+
+      receivers = receivers - [gift_receiver]
     end
 
     flash[:success] = 'The players have been assigned!'
-    redirect_to "#{secret_santa_games_path}/#{@secret_santa_user.secret_santa_game_id}"
+    redirect_to "#{secret_santa_games_path}/#{secret_santa_user_game_params}"
   end
   
   private
@@ -38,4 +37,8 @@ class SecretSantaUsersController < ApplicationController
   def user_games_params
     params.require(:secret_santa_user).permit(:user_id, :secret_santa_game_id, :gift_receiver)
   end 
+
+  def secret_santa_user_game_params
+    params[:secret_santa_user][:game_id].to_i
+  end
 end
